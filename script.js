63f614661d6e5724d95e62628eb0c2d2863ee84f
Gabriel <gabrigomez15@gmail.com>
2023-04-27 16:33:20 -0300

    feat: add basic score

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const runner = document.querySelector('.runner');
 const barrier = document.querySelector('.barrier');
 const city = document.querySelector('.city');
 const gameOver = document.querySelector('.game-over');
+const scoreElement = document.querySelector('.score');
 
 const jump = () => {
   runner.classList.add('jump');
@@ -11,18 +12,27 @@ const jump = () => {
   }, 500);
 };
 
-const loop = setInterval(() => {
+let gameOverMode = false;
+let score = 0;
 
+const getScore = setInterval(() => {
+  score = score +1;
+  scoreElement.innerHTML = score;
+
+  if (gameOverMode) {
+    clearInterval(getScore);
+  }
+}, 200);
+
+const loop = setInterval(() => {
   const barrierPosition = barrier.offsetLeft;
   const runnerPosition = +window.getComputedStyle(runner).bottom.replace('px', '');
 
-  
   if(barrierPosition <= 120 && barrierPosition > 0 && runnerPosition <= 100 ) {
     city.style.animation = 'none';
     city.classList.remove('city');
     city.classList.add('city-over');
     gameOver.style.opacity = '1';
-
     
     runner.src = './assets/dead.gif';
     runner.style.marginLeft = '35px';
@@ -31,6 +41,7 @@ const loop = setInterval(() => {
 
     barrier.style.animation = 'none';
     barrier.style.left = `${barrierPosition}px`;
+    gameOverMode = true;
 
     clearInterval(loop);
   }
