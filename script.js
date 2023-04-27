6df1d2eca07305cd16e22e7c13b1cd472e4b18a1
Gabriel <gabrigomez15@gmail.com>
2023-04-27 10:30:49 -0300

    feat: add colision logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const runner = document.querySelector('.runner');
+const barrier = document.querySelector('.barrier');
 
 const jump = () => {
   runner.classList.add('jump');
@@ -6,6 +7,19 @@ const jump = () => {
   setTimeout(() => {
     runner.classList.remove('jump');
   }, 500);
-}
+};
+
+const loop = setInterval(() => {
+
+  const barrierPosition = barrier.offsetLeft;
+  const runnerPosition = +window.getComputedStyle(runner).bottom.replace('px', '');
+
+  
+  if(barrierPosition <= 140 && barrierPosition > 0 && runnerPosition <= 170 ) {
+
+    barrier.style.animation = 'none'
+    barrier.style.left = `${barrierPosition}px`
+  }
+}, 10)
 
 document.addEventListener('keydown', jump);
